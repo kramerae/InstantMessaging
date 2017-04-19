80d7b2e1b8f6cc408808c3d76e1df8ed8e7a380c
amm2297 <amm2297@OBAMA>
2017-04-19 17:35:18 -0500

    Chat

--- FinalProject/FinalProject/Chat.Designer.cs
+++ FinalProject/FinalProject/Chat.Designer.cs
@@ -28,19 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.MessageBox = new System.Windows.Forms.ListBox();
+            this.ChatTextBox = new System.Windows.Forms.TextBox();
+            this.uxSend = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // MessageBox
+            // 
+            this.MessageBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MessageBox.FormattingEnabled = true;
+            this.MessageBox.ItemHeight = 16;
+            this.MessageBox.Location = new System.Drawing.Point(12, 12);
+            this.MessageBox.Name = "MessageBox";
+            this.MessageBox.Size = new System.Drawing.Size(583, 244);
+            this.MessageBox.TabIndex = 0;
+            // 
+            // ChatTextBox
+            // 
+            this.ChatTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ChatTextBox.Location = new System.Drawing.Point(12, 269);
+            this.ChatTextBox.Name = "ChatTextBox";
+            this.ChatTextBox.Size = new System.Drawing.Size(477, 26);
+            this.ChatTextBox.TabIndex = 1;
+            // 
+            // uxSend
+            // 
+            this.uxSend.BackColor = System.Drawing.Color.Lime;
+            this.uxSend.Font = new System.Drawing.Font("Berlin Sans FB", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.uxSend.Location = new System.Drawing.Point(495, 268);
+            this.uxSend.Name = "uxSend";
+            this.uxSend.Size = new System.Drawing.Size(100, 27);
+            this.uxSend.TabIndex = 2;
+            this.uxSend.Text = "SEND";
+            this.uxSend.UseVisualStyleBackColor = false;
             // 
             // Chat
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(607, 358);
+            this.ClientSize = new System.Drawing.Size(607, 303);
+            this.Controls.Add(this.uxSend);
+            this.Controls.Add(this.ChatTextBox);
+            this.Controls.Add(this.MessageBox);
             this.Name = "Chat";
             this.Text = "Chat";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox MessageBox;
+        private System.Windows.Forms.TextBox ChatTextBox;
+        private System.Windows.Forms.Button uxSend;
     }
 }
